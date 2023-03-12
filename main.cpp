0a7c36ad4858a4dec6cb48052c112dbe60ab87c1
Schmidt Anton <schmidt.ajax@yandex.ru>
2023-03-12 18:02:37 +0700

    WIP 132/133 testcases passed
    Time Limit Exceeded

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include <vector>
+#include <cassert>
 
 using namespace std;
+
+#define APPEND(head, tail, val)           \
+    if (tail) {                           \
+        (tail)->next = new ListNode(val); \
+        (tail) = (tail)->next;            \
+    } else {                              \
+        head = tail = new ListNode(val);  \
+    }
 
 struct ListNode {
   int val;
@@ -11,15 +20,82 @@ struct ListNode {
   ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+ListNode *MakeList(const vector<int> &nums) {
+  ListNode *head = nullptr;
+  for (auto iter = nums.rbegin(); iter != nums.rend(); ++iter) {
+    head = new ListNode(*iter, head);
+  }
+
+  return head;
+}
+
 class Solution {
+  ListNode *mergeLists(ListNode *l1, ListNode *l2) {
+    ListNode *result_head = nullptr;
+    ListNode *result_tail = nullptr;
+    while (l1 && l2) {
+      int minval;
+      if (l1->val < l2->val) {
+        minval = l1->val;
+        l1 = l1->next;
+      } else {
+        minval = l2->val;
+        l2 = l2->next;
+      }
+      APPEND(result_head, result_tail, minval);
+    }
+
+    while (l1) {
+      APPEND(result_head, result_tail, l1->val);
+      l1 = l1->next;
+    }
+
+    while (l2) {
+      APPEND(result_head, result_tail, l2->val);
+      l2 = l2->next;
+    }
+
+    return result_head;
+  }
+
  public:
   ListNode *mergeKLists(vector<ListNode *> &lists) {
-    // FIXME
-    return nullptr;
+    while (1 < lists.size()) {
+      lists[0] = mergeLists(lists[0], lists[1]);
+      std::swap(lists[1], lists.back());
+      lists.pop_back();
+    }
+
+    return !lists.empty() ? lists.front() : nullptr;
   }
 };
 
+void Test1() {
+  Solution s;
+  vector<ListNode *> lists{MakeList({1, 4, 5}), MakeList({1, 3, 4}), MakeList({2, 6})};
+  ListNode *result = s.mergeKLists(lists);
+  for (; result; result = result->next) {
+    cout << result->val << ' ';
+  }
+  cout << '\n';
+}
+
+void Test2() {
+  Solution s;
+  vector<ListNode *> lists{};
+  assert(nullptr == s.mergeKLists(lists));
+}
+
+void Test3() {
+  Solution s;
+  vector<ListNode *> lists{nullptr};
+  assert(nullptr == s.mergeKLists(lists));
+}
+
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  Test1();
+  Test2();
+  Test3();
+
   return 0;
 }
